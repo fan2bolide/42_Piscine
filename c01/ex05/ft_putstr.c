765b855fbec5d32dd4ec9fe3a7f5f7969f69a87f
fan2bolide <82985749+fan2bolide@users.noreply.github.com>
2021-08-17 16:13:11 +0200

    c01 upload after moulinette (60%)

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjeannot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/08 14:36:05 by bjeannot          #+#    #+#             */
+/*   Updated: 2021/08/08 14:36:06 by bjeannot         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjeannot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/08 15:31:44 by bjeannot          #+#    #+#             */
+/*   Updated: 2021/08/08 15:31:45 by bjeannot         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size);
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i <= size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = temp;
+		i++;
+	}
+}
